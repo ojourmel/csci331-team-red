aabe33bb671098ef980c731d98d198ab76e77565
ojourmel <jourmeob@csci.viu.ca>
2013-10-19 21:36:51 -0700

    more skeleton code

---
+++ src/main/java/csci331/team/red/shared/Result.java
@@ -0,0 +1,17 @@
+package csci331.team.red.shared;
+
+/**
+ * The result of a database query from the player.<br>
+ * FIXME: <b>Stub</b> <br>
+ * FIXME: Possible naming conflicts with actual database queries...
+ * 
+ * @author ojourmel
+ */
+public class Result {
+	public static final Result INVALID = new Result(-1);
+
+	public Result(int id) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
